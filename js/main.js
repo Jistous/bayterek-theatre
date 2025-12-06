f0a751ef2d657dc3a33df8065826a3627a96c492
Jistous <79151967+Jistous@users.noreply.github.com>
2025-12-07 00:10:35 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -76,3 +76,8 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+document.querySelectorAll('a[href^="http"]').forEach(a => {
+  a.setAttribute("target", "_blank");
+  a.setAttribute("rel", "noopener");
+});
